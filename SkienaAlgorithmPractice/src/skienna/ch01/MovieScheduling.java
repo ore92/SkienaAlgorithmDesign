983399c591d1f59d43c22cdb2882b2224d046406
Ore Asonibare <ore@Ores-MacBook.local>
2020-11-05 16:23:38 -0800

    Movie Interval Scheduling

--- SkienaAlgorithmPractice/src/skienna/ch01/MovieScheduling.java
+++ SkienaAlgorithmPractice/src/skienna/ch01/MovieScheduling.java
@@ -1,7 +1,7 @@
 package skienna.ch01;
 
+import java.util.ArrayList;
 import java.util.Collections;
-
 import java.util.List;
 
 /**
@@ -13,23 +13,87 @@ import java.util.List;
  *
  */
 public class MovieScheduling {
-
-	public static void earliestEndTimeHeuristic(List<Interval> movies) {
-		Collections.sort(movies, (movie1,movie2) -> Double.compare(movie1.getEnd(), movie2.getEnd()));
-		double excludeCondition = Double.NEGATIVE_INFINITY;
-		
-		int i=0, j=-1;		
-		while(i < movies.size()) {
-			if(movies.get(i).getStart() > excludeCondition) {
+	
+	/**
+	 * Returns a subset of non-overlapping intervals using the earliest time
+	 * heuristic. This heuristic is not optimal for returning the largest possible
+	 * non overlapping subset
+	 * 
+	 * @param movies the movie intervals
+	 */
+	public static void earliestStartTimeHeuristic(List<Interval> movies) {
+		Collections.sort(movies, (movie1, movie2) -> Double.compare(movie1.getStart(), movie2.getStart()));
+		double inclusionThreshold = Double.NEGATIVE_INFINITY;
+		int i = 0, j = -1;
+		while (i < movies.size()) {
+			boolean inclusionCondition = movies.get(i).getStart() > inclusionThreshold;
+			if (inclusionCondition) {
 				j++;
 				movies.set(j, movies.get(i));
-				excludeCondition = movies.get(j).getEnd();
+				inclusionThreshold = movies.get(j).getEnd();
 			}
 			i++;
 		}
-		
 		i--;
-		while(i > j) {
+		while (i > j) {
+			movies.remove(i);
+			i--;
+		}
+	}
+	
+	/**
+	 * Returns a subset of non-overlapping intervals using the shortest interval
+	 * heuristic. This heuristic is not optimal for returning the largest possible
+	 * non-overlapping set
+	 * 
+	 * @param movies the movie intervals
+	 * @return
+	 */
+	public static List<Interval> shortestIntervalHeuristic(List<Interval> movies) {
+		Collections.sort(movies, (movie1, movie2) -> Double.compare(movie1.getEnd() - movie1.getStart(),
+				movie2.getEnd() - movie2.getStart()));
+		List<Interval> includedIntervals = new ArrayList<Interval>();
+		for (int i = 0; i < movies.size(); i++) {
+			boolean inclusionCondition = true;
+			for (int j = 0; j < includedIntervals.size(); j++) {
+				boolean exclusionCriteria = movies.get(i).getEnd() > movies.get(j).getStart()
+						&& movies.get(i).getStart() < movies.get(j).getEnd();
+
+				if (exclusionCriteria) {
+					inclusionCondition = false;
+					break;
+				}
+
+			}
+			if (inclusionCondition) {
+				includedIntervals.add(movies.get(i));
+			}
+		}
+		return includedIntervals;
+	}
+	
+	/**
+	 * Returns a subset of non-overlapping movie intervals using the optimal
+	 * end-time heuristic
+	 * 
+	 * @param movies the movie intervals
+	 */
+	public static void earliestEndTimeHeuristic(List<Interval> movies) {
+		Collections.sort(movies, (movie1, movie2) -> Double.compare(movie1.getEnd(), movie2.getEnd()));
+		double inclusionCondition = Double.NEGATIVE_INFINITY;
+
+		int i = 0, j = -1;
+		while (i < movies.size()) {
+			if (movies.get(i).getStart() > inclusionCondition) {
+				j++;
+				movies.set(j, movies.get(i));
+				inclusionCondition = movies.get(j).getEnd();
+			}
+			i++;
+		}
+
+		i--;
+		while (i > j) {
 			movies.remove(i);
 			i--;
 		}
--- SkienaAlgorithmPractice/tst/skienna/ch01/MovieSchedulingTest.java
+++ SkienaAlgorithmPractice/tst/skienna/ch01/MovieSchedulingTest.java
@@ -27,19 +27,55 @@ public class MovieSchedulingTest {
 		MovieScheduling.earliestEndTimeHeuristic(test1);
 		
 		assertEquals(2, test1.size());
-		Collections.sort(test1, (interval1, interval2) -> Double.compare(interval1.getStart(),interval2.getEnd()));
-		assertEquals(test1, Arrays.asList(new Interval(0,3), new Interval(4,6)));
+		Collections.sort(test1, (interval1, interval2) -> Double.compare(interval1.getStart(),interval2.getStart()));
+		assertEquals(Arrays.asList(new Interval(0,3), new Interval(4,6)), test1);
 		
 		MovieScheduling.earliestEndTimeHeuristic(test2);
 		
 		assertEquals(2, test2.size());
-		Collections.sort(test2, (interval1, interval2) -> Double.compare(interval1.getStart(),interval2.getEnd()));
-		assertEquals(test2, Arrays.asList(new Interval(0,3), new Interval(4,5)));
+		Collections.sort(test2, (interval1, interval2) -> Double.compare(interval1.getStart(),interval2.getStart()));
+		assertEquals(Arrays.asList(new Interval(0,3), new Interval(4,5)), test2);
 		
 		MovieScheduling.earliestEndTimeHeuristic(test3);
 		
 		assertEquals(2, test3.size());
-		Collections.sort(test3, (interval1, interval2) -> Double.compare(interval1.getStart(),interval2.getEnd()));
-		assertEquals(test3, Arrays.asList(new Interval(1,4), new Interval(5,7)));
+		Collections.sort(test3, (interval1, interval2) -> Double.compare(interval1.getStart(),interval2.getStart()));
+		assertEquals(Arrays.asList(new Interval(1,4), new Interval(5,7)), test3);
+	}
+	
+	@Test
+	public void testEarliestStartTimeHeuristic() {
+		List<Interval> test1 = new ArrayList<>(Arrays.asList(new Interval(0,3), new Interval(1,5), new Interval (4,6)));
+		List<Interval> test2 = new ArrayList<>(Arrays.asList(new Interval(0,9), new Interval(1,4), new Interval(5,7)));
+		
+		MovieScheduling.earliestStartTimeHeuristic(test1);
+		
+		assertEquals(2, test1.size());
+		Collections.sort(test1, (interval1, interval2) -> Double.compare(interval1.getStart(),interval2.getStart()));
+		assertEquals(Arrays.asList(new Interval(0,3), new Interval(4,6)), test1);
+		
+        MovieScheduling.earliestStartTimeHeuristic(test2);
+		
+		assertEquals(1, test2.size());
+		Collections.sort(test2, (interval1, interval2) -> Double.compare(interval1.getStart(),interval2.getStart()));
+		assertEquals(Arrays.asList(new Interval(0,9)), test2);
+	}
+	
+	@Test
+	public void testShortestIntervalHeuristic() {
+		List<Interval> test1 = new ArrayList<>(Arrays.asList(new Interval(0,5), new Interval(4,7), new Interval (6,10)));
+		List<Interval> test2 = new ArrayList<>(Arrays.asList(new Interval(0,9), new Interval(1,4), new Interval(5,7)));
+		
+		List<Interval> solution1 = MovieScheduling.shortestIntervalHeuristic(test1);
+		
+		assertEquals(1, solution1.size());
+		Collections.sort(solution1, (interval1, interval2) -> Double.compare(interval1.getStart(),interval2.getEnd()));
+		assertEquals(Arrays.asList(new Interval(4,7)), solution1);
+		
+		List<Interval> solution2 = MovieScheduling.shortestIntervalHeuristic(test2);
+		
+		assertEquals(2, solution2.size());
+		Collections.sort(solution2, (interval1, interval2) -> Double.compare(interval1.getStart(),interval2.getStart()));
+		assertEquals(Arrays.asList(new Interval(1,4), new Interval(5,7)), solution2);
 	}
 }
